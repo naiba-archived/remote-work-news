0f5bb1e8f2f6f7d6e6ae2a25c93061ac0ab94984
奶爸 <hi@nai.ba>
2019-05-30 20:39:13 +0800

    css selector

--- crawlers/yuancheng_work.go
+++ crawlers/yuancheng_work.go
@@ -23,7 +23,7 @@ func (y *YuanChengDotWorkCrawler) FetchNews() ([]rwn.News, error) {
 		newsItem.MediaID = 6
 		title := s.Find("div.job-body").First()
 		link := title.Find("a.job-title").First()
-		newsItem.Title = "「" + title.Find("span.label-primary").Text() + "」" + link.Text()
+		newsItem.Title = "「" + title.Find("b.text-outstand").Text() + "」" + link.Text()
 		newsItem.URL = link.AttrOr("href", "")
 		newsItem.Pusher = title.Find("small").First().Text()
 		newsItem.PublishedAt, _ = time.Parse("2006-01-02 15:04:05",
